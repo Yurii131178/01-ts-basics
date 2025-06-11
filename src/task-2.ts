c6328d8a4cd839e33e16022f5d91e0cde943d039
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-06-12 00:10:49 +0300

    recap with taskes added

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,7 +1,27 @@
+// interface Product {
+//     readonly id: number;
+//     title: string;
+//     description?: string;
+// }
+
+// const product: Product = {
+//   id: 1,
+//   title: "Tablet",
+//   description: "Compact and fast",
+// };
+
+// console.log(`Product: ${JSON.stringify(product)}`);
+
+//////////////////////////////////////////////////////////////
+
+// Задача 2. Інтерфейси (файл task-2.ts)
+
+// У цьому фрагменті коду є об'єкт product, який описує товар.
+
 interface Product {
-    readonly id: number;
-    title: string;
-    description?: string;
+  readonly id: number;
+  title: string;
+  description?: string;
 }
 
 const product: Product = {
@@ -11,3 +31,11 @@ const product: Product = {
 };
 
 console.log(`Product: ${JSON.stringify(product)}`);
+
+// Завдання:
+
+// Створи інтерфейс Product, який описує структуру цього об’єкта.
+// Зроби поле id тільки для читання – воно не повинно змінюватись після створення об’єкта.
+// Зроби поле description необов’язковим – не всі товари можуть його мати.
+// Типізуй змінну product за допомогою створеного інтерфейсу.
+// Залиш властивості обʼєкта product і їх значення без змін.
--- src/task-4.ts
+++ src/task-4.ts
@@ -9,3 +9,29 @@ function printUserInfo(name: string, age: number, email?: string): void {
 printUserInfo("Alice", 30);
 printUserInfo("Bob", 25, "bob@mail.com");
 
+////////////////////////////////////////////////
+// Задача 4. Функції (файл task-4.ts)
+// Функція printUserInfo виводить інформацію про користувача, включаючи ім’я, вік і (опціонально) email.
+
+// interface User {
+//   name: string;
+//   age: number;
+//   email: string;
+// }
+
+// function printUserInfo(name: string, age: number, email?: string): void {
+//   console.log("Name:", name);
+//   console.log("Age:", age);
+//   if (email) {
+//     console.log("Email:", email);
+//   }
+// }
+
+// printUserInfo("Alice", 30);
+// printUserInfo("Bob", 25, "bob@mail.com");
+
+// Завдання:
+// Додай явну типізацію до параметрів функції: name, age та email.
+// Зроби параметр email опціональним – щоб функція могла працювати як з ним, так і без нього.
+// Типізуй повернення функції, зауваж що вона нічого явно не повертає.
+// Залиш реалізацію функції без змін.
